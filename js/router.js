362199d8b3e6a92c26e83b0e601aa08570ff9cd2
Daniel Subasic <subasic97@gmail.com>
2019-12-11 22:17:36 +0100

    fixade routes igen

--- js/router.js
+++ js/router.js
@@ -1,7 +1,7 @@
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/vue1', component: Home },
-        { path: '/vue1/foo', component: Foo }
+        { path: '/vue1/', component: Home },
+        { path: '/vue1/foo/', component: Foo }
     ]
 })
