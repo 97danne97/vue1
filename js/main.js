9347c4f7c4667385afc9e171064c3397237c1749
Daniel Subasic <subasic97@gmail.com>
2019-12-15 18:14:53 +0100

    Flyttade js-filer till egen mapp

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const Main = window.httpVueLoader('components/Main.vue')
+const Riksdagen = window.httpVueLoader('components/Riksdagen.vue')
+const Navbar = window.httpVueLoader('components/default/Navbar.vue')
+const Drawer = window.httpVueLoader('components/default/Drawer.vue')
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Riksdagen
+        }
+    ]
+})
+
+new Vue({
+    router,
+    el: '#app',
+    components: {
+        Main
+    },
+    template: '<Main></Main>'
+})
