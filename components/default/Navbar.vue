392d620168992852a4e30fa34edaa73223d93339
Daniel Subasic <subasic97@gmail.com>
2019-12-17 02:00:49 +0100

    Nav är nu fixed

--- components/default/Navbar.vue
+++ components/default/Navbar.vue
@@ -1,17 +1,19 @@
 <template>
-    <nav>
-        <div class="nav-wrapper">
-            <a href="#" data-target="sidenav-1" class="sidenav-trigger left">
-                <i class="material-icons">menu</i>
-            </a>
-            <router-link tag="a" class="brand-logo" to="/">Politiknörden</router-link>
-            <ul id="nav-mobile" class="right hide-on-small-and-down">
-                <li>
-                    <router-link to="/">Politiknörden</router-link>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" data-target="sidenav-1" class="sidenav-trigger left">
+                    <i class="material-icons">menu</i>
+                </a>
+                <router-link tag="a" class="brand-logo" to="/">Politiknörden</router-link>
+                <ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li>
+                        <router-link to="/">Politiknörden</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -19,7 +21,8 @@ module.exports = {
     components: {
         Drawer
     },
-    mounted() { //jQuery för att skapa sidenav-instans
+    mounted() {
+        //jQuery för att skapa sidenav-instans
         $(document).ready(function() {
             $(".sidenav").sidenav();
         });
@@ -28,7 +31,7 @@ module.exports = {
 </script>
 
 <style scoped>
-.brand-logo{
+.brand-logo {
     margin-left: 10px;
 }
 </style>
