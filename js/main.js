2f1448bb5b0d4cea53b1848d3e294e7f1f689a54
Daniel Subasic <subasic97@gmail.com>
2019-12-18 00:24:25 +0100

    La till router för ledamot-sida

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
+Vue.use(VueRouter)
 const Main = window.httpVueLoader('components/Main.vue')
 const Riksdagen = window.httpVueLoader('components/Riksdagen.vue')
 const Navbar = window.httpVueLoader('components/default/Navbar.vue')
 const Drawer = window.httpVueLoader('components/default/Drawer.vue')
 const Footer1 = window.httpVueLoader('components/default/Footer1.vue')
+const Ledamot = window.httpVueLoader('components/Ledamot.vue')
 
-Vue.use(VueRouter)
+
 Vue.use(Vue2Filters)
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: 'hash',
+    base:'/',
     routes: [
         {
-            path: '/',
-            component: Riksdagen
+            path: '/riksdagen',
+            component: Riksdagen,
+            alias: '/',
+            /* children:[
+                {
+                    path: '/ledamot/:id',
+                    component: Ledamot,
+                },
+            ] */
+        },
+        {
+            path: '/ledamot/:id',
+            component:Ledamot
         }
+        
     ]
 })
 
@@ -23,5 +38,5 @@ new Vue({
     components: {
         Main
     },
-    template: '<Main></Main>',
+    template: '<Main></Main>'
 })
