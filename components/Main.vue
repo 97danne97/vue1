8f52c2be68d313da81bc1056ec66915c5451cea0
Daniel Subasic <subasic97@gmail.com>
2019-12-18 00:22:27 +0100

    Hämtar api data från Main som nu kan skickas till komponenter som behöver den

--- components/Main.vue
+++ components/Main.vue
@@ -1,42 +1,96 @@
 <template>
-    <div>
+    <div id="main">
         <Navbar></Navbar>
         <Drawer></Drawer>
-        <transition name="fade">
-            <router-view tag="div" class="container"></router-view>
+        <transition :name="transitionName">
+            <router-view tag="div" id="main-content" :data="data" :response="response"/>
         </transition>
-        <Footer1></Footer1>
     </div>
 </template>
 <script>
 module.exports = {
+    data: function(){
+        return{
+            data:'',
+            response:false
+        }
+    },
     components: {
         Navbar,
         Drawer,
         Footer1
+    },
+    watch: { // Väljer animation beroende på om djupet på URL:en är. Ger en känsla av att man går fram och tillbaka.
+        $route(to, from) {
+            const toDepth = to.path.split("/").length;
+            const fromDepth = from.path.split("/").length;
+            this.transitionName =
+                toDepth < fromDepth ? "slide-right" : "slide-left";
+        }
+    },
+    mounted() {
+        //GET-request till API, lagrar svar i info
+        axios
+            .get("https://data.riksdagen.se/personlista/?utformat=json")
+            .then(res => {
+                this.onResponse(res); // Kallar på onResponse vid svar från API
+            });
+    },
+    methods:{
+        onResponse: function(res) {
+            this.data = res.data.personlista.person.splice(0,50); //begränsa
+            this.response = true;
+        }
     }
 };
 </script>
 <style scoped>
+#main{
+    overflow-x: hidden;
+}
+#main-content{
+    min-height: 100vh;
+}
 
 /* fade-animation för element i <transition>-taggar (router-views) */
-.fade-enter-active,
-.fade-leave-active {
+.slide-right-enter-active,
+.slide-right-leave-active {
     transition: 0.2s ease;
 }
 
-.fade-enter-active {
+.slide-right-enter-active {
     transition-delay: 0.2s;
-    transform-origin: top left;
     height: 0;
 }
 
-.fade-enter {
+.slide-right-enter {
     opacity: 0;
     transform: translateX(-100px);
 }
-.fade-leave-active {
+.slide-right-leave-active {
     opacity: 0;
+    transform: translateX(100px);
+    height: 0;
+}
+
+/* fade-animation för element i <transition>-taggar (router-views) */
+.slide-left-enter-active,
+.slide-left-leave-active {
+    transition: 0.2s ease;
+}
+
+.slide-left-enter-active {
+    transition-delay: 0.2s;
+    height: 0;
+}
+
+.slide-left-enter {
+    opacity: 0;
+    transform: translateX(100px);
+}
+.slide-left-leave-active {
+    opacity: 0;
+    transform: translateX(-100px);
     height: 0;
 }
 </style>
